a2e56a65f16f4539fa0cdd7bed01f403208749e3
obigu <torinthechosen@gmail.com>
2020-10-17 11:40:07 +0200

    Remove optional Vibration permission from manifest and added inline supress lint warning annotations

--- backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/DefaultAndroidInput.java
+++ backends/gdx-backend-android/src/com/badlogic/gdx/backends/android/DefaultAndroidInput.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2011 See AUTHORS file.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,6 +16,7 @@
 
 package com.badlogic.gdx.backends.android;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -51,7 +52,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /** An implementation of the {@link Input} interface for Android.
- * 
+ *
  * @author mzechner */
 /** @author jshapcot */
 public class DefaultAndroidInput extends AbstractInput implements AndroidInput {
@@ -648,16 +649,19 @@ public class DefaultAndroidInput extends AbstractInput implements AndroidInput {
 	}
 
 	@Override
+	@SuppressLint("MissingPermission")
 	public void vibrate (int milliseconds) {
 		haptics.vibrate(milliseconds);
 	}
 
 	@Override
+	@SuppressLint("MissingPermission")
 	public void vibrate (int milliseconds, boolean fallback) {
 		haptics.vibrate(milliseconds);
 	}
 
 	@Override
+	@SuppressLint("MissingPermission")
 	public void vibrate (int milliseconds, int amplitude, boolean fallback) {
 		haptics.vibrate(milliseconds, amplitude, fallback);
 	}
