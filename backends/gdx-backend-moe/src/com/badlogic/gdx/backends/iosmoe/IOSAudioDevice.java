36241bb5341ae12e58aa697a58423447a74c18f2
Berstanio <berstaniodev@gmail.com>
2025-02-27 12:53:03 +0100

    feat: MOE 1.14.2

---
+++ backends/gdx-backend-moe/src/com/badlogic/gdx/backends/iosmoe/IOSAudioDevice.java
@@ -0,0 +1,158 @@
+
+package com.badlogic.gdx.backends.iosmoe;
+
+import com.badlogic.gdx.audio.AudioDevice;
+import com.badlogic.gdx.backends.iosmoe.objectal.ALBuffer;
+import com.badlogic.gdx.backends.iosmoe.objectal.ALSource;
+import com.badlogic.gdx.backends.iosmoe.objectal.OALAudioSession;
+import org.moe.natj.general.ptr.ShortPtr;
+import org.moe.natj.general.ptr.impl.PtrFactory;
+
+import java.nio.ShortBuffer;
+import java.util.ArrayList;
+
+import static apple.openal.c.OpenAL.*;
+
+public class IOSAudioDevice implements AudioDevice {
+
+	private ALSource alSource;
+	private ArrayList<ALBuffer> alBuffers = new ArrayList<>();
+	private ArrayList<ALBuffer> alBuffersFree = new ArrayList<>();
+	private int samplingRate;
+	private boolean isMono;
+	private int format;
+	private ShortBuffer tmpBuffer;
+	private int minSize;
+	private int latency;
+
+	public IOSAudioDevice (int samplingRate, boolean isMono, int minSize, int bufferCount) {
+		this.samplingRate = samplingRate;
+		this.isMono = isMono;
+		this.format = isMono ? 0x1101 : 0x1103;
+		this.minSize = minSize;
+		// This will use the native byte order. On iOS this should always be little endian.
+		// This is relevant because it might be, that iOS OpenAL only supports little endian, contrary to the OpenAL specification.
+		tmpBuffer = ShortBuffer.allocate(minSize);
+		latency = minSize / (isMono ? 1 : 2) / bufferCount;
+		alSource = ALSource.alloc().init();
+		for (int i = 0; i < bufferCount; i++) {
+			// We use ALBuffer here to keep as close to ObjectAL as possible. But this is just hackery to generate a bufferid in the
+			// end
+			ALBuffer buffer = ALBuffer.alloc().initWithNameDataSizeFormatFrequency("test",
+				PtrFactory.newWeakShortArray(1, (short)100), 2, format, samplingRate);
+			alBuffersFree.add(buffer);
+		}
+	}
+
+	@Override
+	public boolean isMono () {
+		return isMono;
+	}
+
+	@Override
+	public void writeSamples (short[] samples, int offset, int numSamples) {
+		ShortPtr voidPtr;
+		if (numSamples + tmpBuffer.position() >= minSize) {
+			// We can now process the data from the temp buffer
+			voidPtr = PtrFactory.newShortArray(numSamples + tmpBuffer.position());
+
+			voidPtr.copyFrom(tmpBuffer.array(), 0, 0, tmpBuffer.position());
+			voidPtr.copyFrom(samples, offset, tmpBuffer.position(), numSamples);
+			numSamples += tmpBuffer.position();
+			tmpBuffer.position(0);
+		} else {
+			tmpBuffer.put(samples, offset, numSamples);
+			return;
+		}
+
+		if (alBuffersFree.isEmpty()) {
+			while (true) {
+				// TODO: 14.11.22 Needs proper solution on ObjectAL side
+				if (OALAudioSession.sharedInstance().interrupted()) {
+					try {
+						// Should be a good enough measure
+						Thread.sleep(2);
+					} catch (InterruptedException ignored) {
+					}
+					return;
+				}
+				boolean freedBuffer = false;
+				int toFree = Math.min(alSource.buffersProcessed(), alBuffers.size());
+				for (int j = 0; j < toFree; j++) {
+					ALBuffer alBuffer = alBuffers.get(0);
+					if (alSource.unqueueBuffer(alBuffer)) {
+						alBuffersFree.add(alBuffer);
+						alBuffers.remove(alBuffer);
+						freedBuffer = true;
+					} else {
+						break;
+					}
+				}
+				if (freedBuffer) {
+					break;
+				} else {
+					try {
+						// Should be a good enough measure
+						Thread.sleep(2);
+					} catch (InterruptedException ignored) {
+					}
+				}
+			}
+		}
+		ALBuffer buffer = alBuffersFree.remove(0);
+		alBufferData(buffer.bufferId(), format, voidPtr, numSamples * 2, samplingRate);
+		if (alSource.queueBuffer(buffer)) {
+			alBuffers.add(buffer);
+		}
+		if (!alSource.playing()) {
+			alSource.play();
+		}
+	}
+
+	@Override
+	public void writeSamples (float[] samples, int offset, int numSamples) {
+		short[] shortSamples = new short[samples.length];
+
+		for (int i = offset, j = 0; i < samples.length; i++, j++) {
+			float fValue = samples[i];
+			if (fValue > 1) fValue = 1;
+			if (fValue < -1) fValue = -1;
+			short value = (short)(fValue * Short.MAX_VALUE);
+			shortSamples[j] = value;
+		}
+		writeSamples(shortSamples, offset, numSamples);
+	}
+
+	@Override
+	public int getLatency () {
+		return latency;
+	}
+
+	@Override
+	public void dispose () {
+		alSource.stop();
+		/*
+		 * for (ALBuffer buffer : alBuffersFree) { ObjCRuntime.disposeObject(buffer); } for (ALBuffer buffer : alBuffers) {
+		 * ObjCRuntime.disposeObject(buffer); } ObjCRuntime.disposeObject(alSource);
+		 */
+		// Maybe let GC handle the disposing?
+		alBuffers = null;
+		alBuffersFree = null;
+		alSource = null;
+	}
+
+	@Override
+	public void setVolume (float volume) {
+		alSource.setVolume(volume);
+	}
+
+	@Override
+	public void pause () {
+		if (alSource != null) alSource.setPaused(true);
+	}
+
+	@Override
+	public void resume () {
+		alSource.setPaused(false);
+	}
+}
---
+++ backends/gdx-backend-moe/src/com/badlogic/gdx/backends/iosmoe/IOSGraphics.java
@@ -0,0 +1,627 @@
+/*******************************************************************************
+ * Copyright 2011 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package com.badlogic.gdx.backends.iosmoe;
+
+import apple.corefoundation.struct.CGPoint;
+import apple.corefoundation.struct.CGSize;
+import apple.uikit.UIScreen;
+import apple.uikit.struct.UIEdgeInsets;
+import com.badlogic.gdx.AbstractGraphics;
+import com.badlogic.gdx.backends.bindings.metalangle.MGLContext;
+import com.badlogic.gdx.backends.bindings.metalangle.MGLKView;
+import com.badlogic.gdx.backends.bindings.metalangle.MGLKViewController;
+import com.badlogic.gdx.backends.bindings.metalangle.enums.*;
+import com.badlogic.gdx.backends.bindings.metalangle.protocol.MGLKViewControllerDelegate;
+import com.badlogic.gdx.backends.bindings.metalangle.protocol.MGLKViewDelegate;
+import org.moe.natj.general.Pointer;
+import org.moe.natj.general.ann.ByValue;
+import org.moe.natj.objc.ann.Selector;
+
+import com.badlogic.gdx.Application;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Graphics.BufferFormat;
+import com.badlogic.gdx.LifecycleListener;
+import com.badlogic.gdx.backends.iosrobovm.IOSGLES20;
+import com.badlogic.gdx.backends.iosrobovm.IOSGLES30;
+import com.badlogic.gdx.backends.iosmoe.custom.HWMachine;
+import com.badlogic.gdx.graphics.Cursor;
+import com.badlogic.gdx.graphics.Cursor.SystemCursor;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.GL31;
+import com.badlogic.gdx.graphics.GL32;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.glutils.GLVersion;
+import com.badlogic.gdx.graphics.glutils.HdpiMode;
+import com.badlogic.gdx.utils.Array;
+import apple.corefoundation.struct.CGRect;
+import apple.NSObject;
+import apple.corefoundation.struct.CGRect;
+
+public class IOSGraphics extends AbstractGraphics {
+
+	private static final String tag = "IOSGraphics";
+
+	IOSApplication app;
+	IOSInput input;
+	GL20 gl20;
+	GL30 gl30;
+	IOSScreenBounds screenBounds;
+	int safeInsetLeft, safeInsetTop, safeInsetBottom, safeInsetRight;
+	long lastFrameTime;
+	float deltaTime;
+	long framesStart;
+	int frames;
+	int fps;
+	BufferFormat bufferFormat;
+	String extensions;
+
+	private float ppiX = 0;
+	private float ppiY = 0;
+	private float ppcX = 0;
+	private float ppcY = 0;
+	private float density = 1;
+
+	volatile boolean resume = false;
+	volatile boolean appPaused;
+	private long frameId = -1;
+	private boolean isContinuous = true;
+	private boolean isFrameRequested = true;
+
+	private boolean firstFrame = true;
+
+	IOSApplicationConfiguration config;
+	MGLContext context;
+	GLVersion glVersion;
+	IOSGLKView view;
+	IOSUIViewController viewController;
+
+	public IOSGraphics (IOSApplication app, IOSApplicationConfiguration config, IOSInput input, boolean useGLES30) {
+		this.config = config;
+
+		// setup view and OpenGL
+		screenBounds = app.computeBounds();
+
+		if (useGLES30) {
+			context = MGLContext.alloc().initWithAPI(MGLRenderingAPI.GLES3);
+			if (context != null)
+				gl20 = gl30 = new IOSGLES30();
+			else
+				Gdx.app.log("IOGraphics", "OpenGL ES 3.0 not supported, falling back on 2.0");
+		}
+		if (context == null) {
+			context = MGLContext.alloc().initWithAPI(MGLRenderingAPI.GLES2);
+			gl20 = new IOSGLES20();
+			gl30 = null;
+		}
+
+		IOSViewDelegate viewDelegate = IOSViewDelegate.alloc().init();
+		// TODO: 13.06.2022 Solve better
+		viewDelegate.setIosGraphics(this);
+		view = IOSGLKView.alloc().init(new CGRect(new CGPoint(0, 0), new CGSize(screenBounds.width, screenBounds.height)), context);
+		view.setGraphics(this);
+		view.setDelegate(viewDelegate);
+		view.setDrawableColorFormat(config.colorFormat);
+		view.setDrawableDepthFormat(config.depthFormat);
+		view.setDrawableStencilFormat(config.stencilFormat);
+		view.setDrawableMultisample(config.multisample);
+		view.setMultipleTouchEnabled(true);
+
+		viewController = app.createUIViewController(this);
+		viewController.setView(view);
+		viewController.setDelegate(viewDelegate);
+
+		int preferredFps;
+		int maxSupportedFPS = (int)(UIScreen.mainScreen().maximumFramesPerSecond());
+		if (config.preferredFramesPerSecond == 0) {
+			preferredFps = maxSupportedFPS;
+		} else {
+			preferredFps = Math.min(config.preferredFramesPerSecond, maxSupportedFPS);
+		}
+		viewController.setPreferredFramesPerSecond(preferredFps);
+
+		this.app = app;
+		this.input = input;
+
+		int r = 0, g = 0, b = 0, a = 0, depth = 0, stencil = 0, samples = 0;
+		if (config.colorFormat == MGLDrawableColorFormat.RGB565) {
+			r = 5;
+			g = 6;
+			b = 5;
+			a = 0;
+		} else {
+			r = g = b = a = 8;
+		}
+		if (config.depthFormat == MGLDrawableDepthFormat.Format16) {
+			depth = 16;
+		} else if (config.depthFormat == MGLDrawableDepthFormat.Format24) {
+			depth = 24;
+		} else {
+			depth = 0;
+		}
+		if (config.stencilFormat == MGLDrawableStencilFormat.Format8) {
+			stencil = 8;
+		}
+		if (config.multisample == MGLDrawableMultisample.Multisample4X) {
+			samples = 4;
+		}
+		bufferFormat = new BufferFormat(r, g, b, a, depth, stencil, samples, false);
+
+		String machineString = HWMachine.getMachineString();
+		IOSDevice device = config.knownDevices.get(machineString);
+		if (device == null) app.error(tag, "Machine ID: " + machineString + " not found, please report to libGDX");
+		int ppi = device != null ? device.ppi : app.guessUnknownPpi();
+		density = ppi / 160f;
+		ppiX = ppi;
+		ppiY = ppi;
+		ppcX = ppiX / 2.54f;
+		ppcY = ppiY / 2.54f;
+		app.debug(tag, "Display: ppi=" + ppi + ", density=" + density);
+
+		// time + FPS
+		lastFrameTime = System.nanoTime();
+		framesStart = lastFrameTime;
+
+		// enable OpenGL
+		makeCurrent();
+		// OpenGL glViewport() function expects backbuffer coordinates instead of logical coordinates
+		gl20.glViewport(0, 0, screenBounds.backBufferWidth, screenBounds.backBufferHeight);
+
+		String versionString = gl20.glGetString(GL20.GL_VERSION);
+		String vendorString = gl20.glGetString(GL20.GL_VENDOR);
+		String rendererString = gl20.glGetString(GL20.GL_RENDERER);
+		glVersion = new GLVersion(Application.ApplicationType.iOS, versionString, vendorString, rendererString);
+
+		appPaused = false;
+	}
+
+	public void resume () {
+		if (!appPaused) return;
+		appPaused = false;
+
+		Array<LifecycleListener> listeners = app.lifecycleListeners;
+		synchronized (listeners) {
+			for (LifecycleListener listener : listeners) {
+				listener.resume();
+			}
+		}
+		resume = true;
+		app.listener.resume();
+	}
+
+	public void pause () {
+		if (appPaused) return;
+		appPaused = true;
+
+		Array<LifecycleListener> listeners = app.lifecycleListeners;
+		synchronized (listeners) {
+			for (LifecycleListener listener : listeners) {
+				listener.pause();
+			}
+		}
+		app.listener.pause();
+	}
+
+	public void draw (MGLKView view, @ByValue CGRect rect) {
+		makeCurrent();
+		// massive hack, GLKView resets the viewport on each draw call, so IOSGLES20
+		// stores the last known viewport and we reset it here...
+		gl20.glViewport(IOSGLES20.x, IOSGLES20.y, IOSGLES20.width, IOSGLES20.height);
+
+		// For default framebuffer, we render a dummy frame during initialization before create
+		// Return early so listener does not process
+		if (firstFrame) {
+			firstFrame = false;
+			return;
+		}
+
+		if (appPaused) {
+			return;
+		}
+
+		long time = System.nanoTime();
+		if (lastFrameTime > time) lastFrameTime = time;
+		if (!resume) {
+			deltaTime = (time - lastFrameTime) / 1000000000.0f;
+		} else {
+			resume = false;
+			deltaTime = 0;
+		}
+		lastFrameTime = time;
+
+		frames++;
+		if (time - framesStart >= 1000000000l) {
+			framesStart = time;
+			fps = frames;
+			frames = 0;
+		}
+
+		input.processEvents();
+		frameId++;
+		app.listener.render();
+	}
+
+	void makeCurrent () {
+		MGLContext.setCurrentContext(context);
+	}
+
+	public void update (MGLKViewController glkViewController) {
+		makeCurrent();
+		app.processRunnables();
+		// pause the GLKViewController render loop if we are no longer continuous
+		// and if we haven't requested a frame in the last loop iteration
+		if (!isContinuous && !isFrameRequested) {
+			viewController.setPaused(true);
+		}
+		isFrameRequested = false;
+	}
+
+	@Override
+	public GL20 getGL20 () {
+		return gl20;
+	}
+
+	@Override
+	public void setGL20 (GL20 gl20) {
+		this.gl20 = gl20;
+		if (gl30 == null) {
+			Gdx.gl = gl20;
+			Gdx.gl20 = gl20;
+		}
+	}
+
+	@Override
+	public boolean isGL30Available () {
+		return gl30 != null;
+	}
+
+	@Override
+	public GL30 getGL30 () {
+		return gl30;
+	}
+
+	@Override
+	public void setGL30 (GL30 gl30) {
+		this.gl30 = gl30;
+		if (gl30 != null) {
+			this.gl20 = gl30;
+
+			Gdx.gl = gl20;
+			Gdx.gl20 = gl20;
+			Gdx.gl30 = gl30;
+		}
+	}
+
+	@Override
+	public boolean isGL31Available () {
+		return false;
+	}
+
+	@Override
+	public GL31 getGL31 () {
+		return null;
+	}
+
+	@Override
+	public void setGL31 (GL31 gl31) {
+
+	}
+
+	@Override
+	public boolean isGL32Available () {
+		return false;
+	}
+
+	@Override
+	public GL32 getGL32 () {
+		return null;
+	}
+
+	@Override
+	public void setGL32 (GL32 gl32) {
+
+	}
+
+	@Override
+	public int getWidth () {
+		if (config.hdpiMode == HdpiMode.Pixels) {
+			return getBackBufferWidth();
+		} else {
+			return screenBounds.width;
+		}
+	}
+
+	@Override
+	public int getHeight () {
+		if (config.hdpiMode == HdpiMode.Pixels) {
+			return getBackBufferHeight();
+		} else {
+			return screenBounds.height;
+		}
+	}
+
+	@Override
+	public int getBackBufferWidth () {
+		return screenBounds.backBufferWidth;
+	}
+
+	@Override
+	public int getBackBufferHeight () {
+		return screenBounds.backBufferHeight;
+	}
+
+	@Override
+	public float getBackBufferScale () {
+		return app.pixelsPerPoint;
+	}
+
+	@Override
+	public float getDeltaTime () {
+		return deltaTime;
+	}
+
+	@Override
+	public int getFramesPerSecond () {
+		return fps;
+	}
+
+	@Override
+	public GraphicsType getType () {
+		return GraphicsType.iOSGL;
+	}
+
+	@Override
+	public GLVersion getGLVersion () {
+		return glVersion;
+	}
+
+	@Override
+	public float getPpiX () {
+		return ppiX;
+	}
+
+	@Override
+	public float getPpiY () {
+		return ppiY;
+	}
+
+	@Override
+	public float getPpcX () {
+		return ppcX;
+	}
+
+	@Override
+	public float getPpcY () {
+		return ppcY;
+	}
+
+	@Override
+	public float getDensity () {
+		return density;
+	}
+
+	@Override
+	public boolean supportsDisplayModeChange () {
+		return false;
+	}
+
+	@Override
+	public DisplayMode[] getDisplayModes () {
+		return new DisplayMode[] {getDisplayMode()};
+	}
+
+	@Override
+	public DisplayMode getDisplayMode () {
+		return new IOSDisplayMode(getWidth(), getHeight(), (int)(viewController.preferredFramesPerSecond()),
+			bufferFormat.r + bufferFormat.g + bufferFormat.b + bufferFormat.a);
+	}
+
+	@Override
+	public Monitor getPrimaryMonitor () {
+		return new IOSMonitor(0, 0, "Primary Monitor");
+	}
+
+	@Override
+	public Monitor getMonitor () {
+		return getPrimaryMonitor();
+	}
+
+	@Override
+	public Monitor[] getMonitors () {
+		return new Monitor[] {getPrimaryMonitor()};
+	}
+
+	@Override
+	public DisplayMode[] getDisplayModes (Monitor monitor) {
+		return getDisplayModes();
+	}
+
+	@Override
+	public DisplayMode getDisplayMode (Monitor monitor) {
+		return getDisplayMode();
+	}
+
+	protected void updateSafeInsets () {
+		safeInsetTop = 0;
+		safeInsetLeft = 0;
+		safeInsetRight = 0;
+		safeInsetBottom = 0;
+
+		UIEdgeInsets edgeInsets = viewController.view().safeAreaInsets();
+		safeInsetTop = (int)edgeInsets.top();
+		safeInsetLeft = (int)edgeInsets.left();
+		safeInsetRight = (int)edgeInsets.right();
+		safeInsetBottom = (int)edgeInsets.bottom();
+		if (config.hdpiMode == HdpiMode.Pixels) {
+			safeInsetTop *= app.pixelsPerPoint;
+			safeInsetLeft *= app.pixelsPerPoint;
+			safeInsetRight *= app.pixelsPerPoint;
+			safeInsetBottom *= app.pixelsPerPoint;
+		}
+	}
+
+	@Override
+	public int getSafeInsetLeft () {
+		return safeInsetLeft;
+	}
+
+	@Override
+	public int getSafeInsetTop () {
+		return safeInsetTop;
+	}
+
+	@Override
+	public int getSafeInsetBottom () {
+		return safeInsetBottom;
+	}
+
+	@Override
+	public int getSafeInsetRight () {
+		return safeInsetRight;
+	}
+
+	@Override
+	public boolean setFullscreenMode (DisplayMode displayMode) {
+		return false;
+	}
+
+	@Override
+	public boolean setWindowedMode (int width, int height) {
+		return false;
+	}
+
+	@Override
+	public void setTitle (String title) {
+	}
+
+	@Override
+	public void setUndecorated (boolean undecorated) {
+	}
+
+	@Override
+	public void setResizable (boolean resizable) {
+	}
+
+	@Override
+	public void setVSync (boolean vsync) {
+	}
+
+	/** Overwrites the preferred framerate for the application. Use {@link IOSApplicationConfiguration#preferredFramesPerSecond}
+	 * instead to set it at application startup.
+	 *
+	 * @param fps the preferred fps */
+	@Override
+	public void setForegroundFPS (int fps) {
+		viewController.setPreferredFramesPerSecond(fps);
+	}
+
+	@Override
+	public BufferFormat getBufferFormat () {
+		return bufferFormat;
+	}
+
+	@Override
+	public boolean supportsExtension (String extension) {
+		if (extensions == null) extensions = Gdx.gl.glGetString(GL20.GL_EXTENSIONS);
+		return extensions.contains(extension);
+	}
+
+	@Override
+	public void setContinuousRendering (boolean isContinuous) {
+		if (isContinuous != this.isContinuous) {
+			this.isContinuous = isContinuous;
+			// start the GLKViewController if we go from non-continuous -> continuous
+			if (isContinuous) viewController.setPaused(false);
+		}
+	}
+
+	@Override
+	public boolean isContinuousRendering () {
+		return isContinuous;
+	}
+
+	@Override
+	public void requestRendering () {
+		isFrameRequested = true;
+		// start the GLKViewController if we are in non-continuous mode
+		// (we should already be started in continuous mode)
+		if (!isContinuous) viewController.setPaused(false);
+	}
+
+	@Override
+	public boolean isFullscreen () {
+		return true;
+	}
+
+	@Override
+	public long getFrameId () {
+		return frameId;
+	}
+
+	@Override
+	public Cursor newCursor (Pixmap pixmap, int xHotspot, int yHotspot) {
+		return null;
+	}
+
+	@Override
+	public void setCursor (Cursor cursor) {
+	}
+
+	@Override
+	public void setSystemCursor (SystemCursor systemCursor) {
+	}
+
+	private static class IOSViewDelegate extends NSObject implements MGLKViewDelegate, MGLKViewControllerDelegate {
+		protected IOSViewDelegate (Pointer peer) {
+			super(peer);
+		}
+
+		private IOSGraphics iosGraphics;
+
+		@Selector("alloc")
+		public static native IOSViewDelegate alloc ();
+
+		@Selector("init")
+		public native IOSViewDelegate init ();
+
+		public void setIosGraphics (IOSGraphics iosGraphics) {
+			this.iosGraphics = iosGraphics;
+		}
+
+		@Override
+		public void mglkViewControllerUpdate (MGLKViewController controller) {
+			iosGraphics.update(controller);
+		}
+
+		@Override
+		public void mglkViewDrawInRect (MGLKView view, CGRect rect) {
+			iosGraphics.draw(view, rect);
+		}
+	}
+
+	private class IOSDisplayMode extends DisplayMode {
+		protected IOSDisplayMode (int width, int height, int refreshRate, int bitsPerPixel) {
+			super(width, height, refreshRate, bitsPerPixel);
+		}
+	}
+
+	private class IOSMonitor extends Monitor {
+		protected IOSMonitor (int virtualX, int virtualY, String name) {
+			super(virtualX, virtualY, name);
+		}
+	}
+}
